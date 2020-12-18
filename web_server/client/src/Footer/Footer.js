8b8abcdb84186310b6ad5c87bf35f5de7781fc0d
Syed Adnan <syedadnandev@gmail.com>
2020-12-18 08:40:12 -0500

    Add Footer.js

---
+++ web_server/client/src/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className='container-fluid footer'>
+        <p>Made by Syed Adnan</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
