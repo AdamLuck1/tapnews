bcb5c1afe2e2a6303ff54464e2dfb630887fe1f6
Syed Adnan <syedadnandev@gmail.com>
2020-12-27 08:40:13 -0500

    Add IntroPage.js

---
+++ web_server/client/src/IntroPage/IntroPage.js
@@ -0,0 +1,31 @@
+import './IntroPage.css';
+
+import { Link } from 'react-router-dom'
+import { Jumbotron, Button } from 'reactstrap';
+
+import Timeline from '../Timeline/Timeline';
+import TechList from '../TechList/TechList';
+import AboutMe from '../AboutMe/AboutMe';
+
+import React from 'react';
+
+class IntroPage extends React.Component {
+  render() {
+    return (
+      <div className='intro_container container'>
+        <Jumbotron className='jumbotron_container'>
+          <h1 className="display-7">Welcome to Tap News</h1>
+          <p className="lead">This is a website where you will find fresh news accommodated for your appetite.</p>
+          <hr className="my-4"></hr>
+          <p>In order for us to recommend news to you, you have to become one of us.</p>
+          <Button color="primary"><Link id='join-us-link' to="/signup">Join us right now</Link></Button>
+        </Jumbotron>
+        <TechList />
+        <Timeline />
+        <AboutMe />
+      </div>
+    );
+  }
+}
+
+export default IntroPage;
